8710512b45f2764129e6a1aa1d41120e0dee8db3
Neuton Foo <neuton.foo@gmail.com>
2022-04-20 20:51:37 -0700

    Initial commit

--- app/main.py
+++ app/main.py
@@ -42,9 +42,3 @@ def people():
     ).json()
 
     return person_json
-
-
-if __name__ == "__main__":
-    # app.run(debug=True)
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port)
